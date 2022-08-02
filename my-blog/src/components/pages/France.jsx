98f7d644ce3189c3383f3d232951ff1bf432bbc0
Nsimone11 <nsimonepettis@gmail.com>
2022-08-02 17:01:50 +0000

    first commit

--- my-blog/src/components/pages/France.jsx
+++ my-blog/src/components/pages/France.jsx
@@ -1,3 +1,4 @@
+import { Grid } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
 
@@ -7,14 +8,22 @@ const France = () => {
     <div>  
      <image src='https://source.unsplash.com/random'></image> 
      
-     <h1>France</h1>
+   <div className="Title"><h1>France</h1></div>
       
-      <p>France, in Western Europe, encompasses medieval cities, alpine villages and Mediterranean beaches. 
-        Paris, its capital, is famed for its fashion houses, classical art museums including the Louvre and 
-        monuments like the Eiffel Tower. The country is also renowned for its wines and sophisticated cuisine. 
-        Lascaux’s ancient cave drawings, Lyon’s Roman theater and the vast Palace of Versailles attest to its 
-       
-        rich history. </p>  
+      <Grid align="center">
+      
+      <div className="France-about">
+        
+        <p>France, in Western Europe, encompasses medieval cities, 
+        alpine villages and Mediterranean beaches. Paris, its capital, is famed for its fashion 
+        houses, classical art museums including the Louvre and monuments like the Eiffel Tower. 
+        The country is also renowned for its wines and sophisticated cuisine. Lascaux’s ancient 
+        cave drawings, Lyon’s Roman theater and the vast Palace of Versailles attest to its rich 
+        history. </p>
+        
+    </div>  
+        </Grid>
+        
         <p id="demo">Click here to get back to the main page.</p>
 
 <Link to="/">
